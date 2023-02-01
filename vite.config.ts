1fa6c21e8acde4f85eae1b9f097c92946d75ada6
叶晨 <cye2@leqee.com>
2023-02-01 20:40:09 +0800

    ✨ trigger github action

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import path from "path";
 import { defineConfig } from "vite";
 import reactRefresh from "@vitejs/plugin-react-refresh";
 
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [reactRefresh()],
